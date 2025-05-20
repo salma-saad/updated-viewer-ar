58ab106644edb7d71285839955b827bd6c7a3b02
mahmoudelsawy99 <mahmoudelsawy048@gmail.com>
2025-05-20 16:44:43 +0300

    build-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // 👇 Your root standalone component
 import { AppComponent } from './app.component';
-import { NgxQRCodeModule } from 'ngx-qrcode2';
 import { RouterModule, Routes } from '@angular/router';
-import { QRCodeComponent } from 'angularx-qrcode';
+import { QRCodeModule } from 'angularx-qrcode';
 import { ArViewerComponent } from './ar-viewer/ar-viewer.component';
 import { QrCodeComponent } from './qr-code/qr-code.component';
 import { HttpClientModule } from '@angular/common/http';
 import { routing } from './app.routes';
 
 const routes: Routes = [
-    { path: '', component: QrCodeComponent }, 
+    { path: '', component: QrCodeComponent },
     { path: 'ar-viewer', component: ArViewerComponent },
     { path: '**', redirectTo: '' }
   ];
-  
+
 @NgModule({
   declarations: [AppComponent, QrCodeComponent],
   imports: [
     BrowserModule,
-    QRCodeComponent,
+    QRCodeModule,
     routing,
     HttpClientModule,
     RouterModule,
   ],
-  exports:[QRCodeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
